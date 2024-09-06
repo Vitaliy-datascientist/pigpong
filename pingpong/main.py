11c8dd8ba3ffb849cf8dc4167ea86ebe4d88842c
Vitaliy-datascientist <vitaliy.dayneko2410@gmail.com>
2024-09-07 02:24:18 +0300

    update(0.1.2)

--- pingpong/main.py
+++ pingpong/main.py
@@ -1,13 +1,16 @@
 import sys
-import pygame
 import random
 import time
+import os
+import pygame
 
 pygame.init()
 
-FONT1 = pygame.font.SysFont('stxingkai', 100)
-FONT2 = pygame.font.SysFont('stxingkai', 50)
-FONT3 = pygame.font.SysFont('stxingkai', 25)
+BASE_DIR = os.path.dirname(__file__)
+
+FONT1 = pygame.font.SysFont("stxingkai", 100)
+FONT2 = pygame.font.SysFont("stxingkai", 50)
+FONT3 = pygame.font.SysFont("stxingkai", 25)
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -17,9 +20,9 @@ GREY = (190, 190, 190)
 
 
 class Screen:
-    icon = pygame.image.load('pingpong/icon.png')
+    icon = pygame.image.load(f"{BASE_DIR}/icon.png")
     pygame.display.set_icon(icon)
-    pygame.display.set_caption('Ping-Pong')
+    pygame.display.set_caption("Ping-Pong")
 
     def __init__(self):
         self.width = 900
@@ -34,29 +37,41 @@ class Screen:
         self.text_menu2_rect = (20, 174)
         self.text_menu3_rect = (20, 278)
         self.text_menu4_rect = (20, 382)
-        self.text_menu1_size = FONT2.size('STANDARD')
-        self.text_menu2_size = FONT2.size('CUSTOM')
-        self.text_menu3_size = FONT2.size('STANDARD WITH BOT')
-        self.text_menu4_size = FONT2.size('CUSTOM WITH BOT')
-        self.text_menu5_size = FONT1.size('PING')
-        self.text_menu6_size = FONT1.size('PONG')
-        self.sound_menu = pygame.mixer.Sound('pingpong/sound_menu.wav')
-        self.sound_game_over = pygame.mixer.Sound('pingpong/game_over_sound.wav')
+        self.text_menu1_size = FONT2.size("STANDARD")
+        self.text_menu2_size = FONT2.size("CUSTOM")
+        self.text_menu3_size = FONT2.size("STANDARD WITH BOT")
+        self.text_menu4_size = FONT2.size("CUSTOM WITH BOT")
+        self.text_menu5_size = FONT1.size("PING")
+        self.text_menu6_size = FONT1.size("PONG")
+        self.sound_menu = pygame.mixer.Sound(f"{BASE_DIR}/sound_menu.wav")
+        self.sound_game_over = pygame.mixer.Sound(f"{BASE_DIR}/game_over_sound.wav")
 
     def menu(self):
-        self.display.blit(FONT2.render('STANDARD', True, GREY), self.text_menu1_rect)
-        self.display.blit(FONT2.render('CUSTOM', True, GREY), self.text_menu2_rect)
-        self.display.blit(FONT2.render('STANDARD WITH BOT', True, GREY), self.text_menu3_rect)
-        self.display.blit(FONT2.render('CUSTOM WITH BOT', True, GREY), self.text_menu4_rect)
-        self.display.blit(FONT1.render('PING', True, RED),
-                          (self.width - 300, (self.height - self.text_menu5_size[1] - 52) // 2))
-        self.display.blit(FONT1.render('PONG', True, BLUE),
-                          (self.width - 320, (self.height - self.text_menu6_size[1] + 52) // 2))
+        self.display.blit(FONT2.render("STANDARD", True, GREY), self.text_menu1_rect)
+        self.display.blit(FONT2.render("CUSTOM", True, GREY), self.text_menu2_rect)
+        self.display.blit(
+            FONT2.render("STANDARD WITH BOT", True, GREY), self.text_menu3_rect
+        )
+        self.display.blit(
+            FONT2.render("CUSTOM WITH BOT", True, GREY), self.text_menu4_rect
+        )
+        self.display.blit(
+            FONT1.render("PING", True, RED),
+            (self.width - 300, (self.height - self.text_menu5_size[1] - 52) // 2),
+        )
+        self.display.blit(
+            FONT1.render("PONG", True, BLUE),
+            (self.width - 320, (self.height - self.text_menu6_size[1] + 52) // 2),
+        )
 
     def blit_item(self, ball, player1, player2):
         self.display.fill(BLACK)
-        self.display.blit(FONT3.render(str(player1.score), True, RED), (self.width // 2 - 30, 10))
-        self.display.blit(FONT3.render(str(player2.score), True, BLUE), (self.width // 2 + 30, 10))
+        self.display.blit(
+            FONT3.render(str(player1.score), True, RED), (self.width // 2 - 30, 10)
+        )
+        self.display.blit(
+            FONT3.render(str(player2.score), True, BLUE), (self.width // 2 + 30, 10)
+        )
         pygame.draw.ellipse(self.display, WHITE, ball.rect)
         pygame.draw.rect(self.display, RED, player1.rect)
         pygame.draw.rect(self.display, BLUE, player2.rect)
@@ -64,9 +79,12 @@ class Screen:
     def end_game(self):
         self.display.fill(BLACK)
         if self.player_blue_win:
-            self.display.blit(FONT1.render('BLUE', True, BLUE), (0, self.height - FONT1.get_height() + 7))
+            self.display.blit(
+                FONT1.render("BLUE", True, BLUE),
+                (0, self.height - FONT1.get_height() + 7),
+            )
         elif self.player_red_win:
-            self.display.blit(FONT1.render('RED', True, RED), (0, 0))
+            self.display.blit(FONT1.render("RED", True, RED), (0, 0))
 
         if self.end_text1:
             self.sound_game_over.play()
@@ -74,19 +92,36 @@ class Screen:
             time.sleep(1)
             self.end_text1 = False
         if self.player_blue_win:
-            self.display.blit(FONT1.render('WIN', True, WHITE), (self.width - FONT1.size('WIN')[0], 0))
+            self.display.blit(
+                FONT1.render("WIN", True, WHITE), (self.width - FONT1.size("WIN")[0], 0)
+            )
         elif self.player_red_win:
-            self.display.blit(FONT1.render('WIN', True, WHITE),
-                              (self.width - FONT1.size('WIN')[0], self.height - FONT1.get_height() + 7))
+            self.display.blit(
+                FONT1.render("WIN", True, WHITE),
+                (
+                    self.width - FONT1.size("WIN")[0],
+                    self.height - FONT1.get_height() + 7,
+                ),
+            )
 
         if self.end_text2:
             pygame.display.flip()
             time.sleep(0.5)
             self.end_text2 = False
-        self.display.blit(FONT2.render('ENTER TO RESTART', True, GREY), (
-            (self.width - FONT2.size('ENTER TO RESTART')[0]) // 2, (self.height - FONT2.get_height() - 50) // 2))
-        self.display.blit(FONT2.render('ESC TO MENU', True, GREY), (
-            (self.width - FONT2.size('ESC TO MENU')[0]) // 2, (self.height - FONT2.get_height() + 50) // 2))
+        self.display.blit(
+            FONT2.render("ENTER TO RESTART", True, GREY),
+            (
+                (self.width - FONT2.size("ENTER TO RESTART")[0]) // 2,
+                (self.height - FONT2.get_height() - 50) // 2,
+            ),
+        )
+        self.display.blit(
+            FONT2.render("ESC TO MENU", True, GREY),
+            (
+                (self.width - FONT2.size("ESC TO MENU")[0]) // 2,
+                (self.height - FONT2.get_height() + 50) // 2,
+            ),
+        )
 
 
 class Ball:
@@ -95,14 +130,16 @@ class Ball:
         self.y = 5
         self.rect = pygame.Rect(445, 245, 10, 10)
         self.speed = [random.choice([-5, 5]), 0]
-        self.sound = pygame.mixer.Sound('pingpong/ball_sound.wav')
+        self.sound = pygame.mixer.Sound(f"{BASE_DIR}/ball_sound.wav")
 
     def move(self):
         self.rect = self.rect.move(self.speed)
 
     def collide_with_players(self, player1, player2):
         if self.not_collide:
-            if self.rect.colliderect(player1.rect) or self.rect.colliderect(player2.rect):
+            if self.rect.colliderect(player1.rect) or self.rect.colliderect(
+                player2.rect
+            ):
                 self.speed[1] = random.choice([-5, -2, -3, -4, -5, 1, 2, 3, 4, 5])
                 self.not_collide = False
                 self.sound.play()
@@ -165,10 +202,10 @@ class Player:
 class Bot(Player):
     def __init__(self, x, y, mode):
         Player.__init__(self, x, y)
-        if mode == 'STANDARD':
+        if mode == "STANDARD":
             self.speed_up = (0, -4)
             self.speed_down = (0, 4)
-        elif mode == 'CUSTOM':
+        elif mode == "CUSTOM":
             self.speed_up = (0, -5)
             self.speed_down = (0, 5)
 
@@ -191,34 +228,50 @@ def menu():
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if (menu_screen.text_menu1_rect[0] <= event.pos[0] <= menu_screen.text_menu1_rect[0] +
-                        menu_screen.text_menu1_size[0]
-                        and menu_screen.text_menu1_rect[1] <= event.pos[1] <= menu_screen.text_menu1_rect[1] +
-                        menu_screen.text_menu1_size[1]):
+                if (
+                    menu_screen.text_menu1_rect[0]
+                    <= event.pos[0]
+                    <= menu_screen.text_menu1_rect[0] + menu_screen.text_menu1_size[0]
+                    and menu_screen.text_menu1_rect[1]
+                    <= event.pos[1]
+                    <= menu_screen.text_menu1_rect[1] + menu_screen.text_menu1_size[1]
+                ):
                     menu_screen.sound_menu.play()
                     menu_on = False
-                    main('STANDARD')
-                elif (menu_screen.text_menu2_rect[0] <= event.pos[0] <= menu_screen.text_menu2_rect[0] +
-                      menu_screen.text_menu2_size[0]
-                      and menu_screen.text_menu2_rect[1] <= event.pos[1] <= menu_screen.text_menu2_rect[1] +
-                      menu_screen.text_menu2_size[1]):
+                    main("STANDARD")
+                elif (
+                    menu_screen.text_menu2_rect[0]
+                    <= event.pos[0]
+                    <= menu_screen.text_menu2_rect[0] + menu_screen.text_menu2_size[0]
+                    and menu_screen.text_menu2_rect[1]
+                    <= event.pos[1]
+                    <= menu_screen.text_menu2_rect[1] + menu_screen.text_menu2_size[1]
+                ):
                     menu_screen.sound_menu.play()
                     menu_on = False
-                    main('CUSTOM')
-                elif (menu_screen.text_menu3_rect[0] <= event.pos[0] <= menu_screen.text_menu3_rect[0] +
-                      menu_screen.text_menu3_size[0]
-                      and menu_screen.text_menu3_rect[1] <= event.pos[1] <= menu_screen.text_menu3_rect[1] +
-                      menu_screen.text_menu3_size[1]):
+                    main("CUSTOM")
+                elif (
+                    menu_screen.text_menu3_rect[0]
+                    <= event.pos[0]
+                    <= menu_screen.text_menu3_rect[0] + menu_screen.text_menu3_size[0]
+                    and menu_screen.text_menu3_rect[1]
+                    <= event.pos[1]
+                    <= menu_screen.text_menu3_rect[1] + menu_screen.text_menu3_size[1]
+                ):
                     menu_screen.sound_menu.play()
                     menu_on = False
-                    main('STANDARD', True)
-                elif (menu_screen.text_menu4_rect[0] <= event.pos[0] <= menu_screen.text_menu4_rect[0] +
-                      menu_screen.text_menu4_size[0]
-                      and menu_screen.text_menu4_rect[1] <= event.pos[1] <= menu_screen.text_menu4_rect[1] +
-                      menu_screen.text_menu4_size[1]):
+                    main("STANDARD", True)
+                elif (
+                    menu_screen.text_menu4_rect[0]
+                    <= event.pos[0]
+                    <= menu_screen.text_menu4_rect[0] + menu_screen.text_menu4_size[0]
+                    and menu_screen.text_menu4_rect[1]
+                    <= event.pos[1]
+                    <= menu_screen.text_menu4_rect[1] + menu_screen.text_menu4_size[1]
+                ):
                     menu_screen.sound_menu.play()
                     menu_on = False
-                    main('CUSTOM', True)
+                    main("CUSTOM", True)
 
         menu_screen.menu()
         pygame.display.update()
@@ -238,9 +291,10 @@ def main(mode, bot=False):
     ball_speed_upy = pygame.USEREVENT + 1
     pygame.time.set_timer(ball_speed_upy, 50000)
     clock = pygame.time.Clock()
-    if mode == 'STANDARD':
+    fps = None
+    if mode == "STANDARD":
         fps = 120
-    elif mode == 'CUSTOM':
+    elif mode == "CUSTOM":
         fps = 80
 
     while game_on:
@@ -248,7 +302,7 @@ def main(mode, bot=False):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if mode == 'CUSTOM':
+            if mode == "CUSTOM":
                 if event.type == ball_speed_upx:
                     ball.speed[0] += 1 if ball.speed[0] > 0 else -1
                 if event.type == ball_speed_upy:
@@ -279,5 +333,5 @@ def main(mode, bot=False):
         clock.tick(fps)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     menu()
